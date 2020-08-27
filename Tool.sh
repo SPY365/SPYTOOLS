3b160c670d71b59a16d089014ed23de318595664
SPY365 <60486117+SPY365@users.noreply.github.com>
2020-08-27 20:11:22 +0600

    Add files via upload

---
+++ Tool.sh
@@ -0,0 +1,13 @@
+echo Tools Created By Jihad Hadan Jibon
+echo Wait A Moment...!
+pkg install git
+pkg install figlet
+pkg install php
+pkg install unzip
+
+echo Successfully Downloaded All Tools
+figlet Success
+echo Downloading Main Framework...
+git clone https://github.com/Stephin-Franklin/T-Phish
+
+figlet Welcome Sinthiya
